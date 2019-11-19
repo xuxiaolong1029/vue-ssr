cc893bbbf4f83a80f7f16013beff15f05de6760e
xuxiaolong1029 <18702515443@163.com>
2019-11-19 08:08:30 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const Vue = require('vue');
+import App from './App';
+import { createRouter } from './router'
 
-module.exports = function createApp(context) {
-  return new Vue({
-    data:{
-      url:context.url
-    },
-    template:`<div>访问的 URL 是： {{ url }}</div>`
+export function createApp() {
+  //创建router实例
+  const router = createRouter();
+  const app = new Vue({
+    // 注入 router 到根 Vue 实例
+    router,
+    render: h => h(App)
   })
-};
+  return { app,router }
+}
