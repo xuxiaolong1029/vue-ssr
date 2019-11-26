52000a8dc421809c99c2a7f7c699323f09638b9c
xuxiaolong1029 <18702515443@163.com>
2019-11-27 00:56:25 +0800

    服务端渲染本地开发环境配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,9 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 module.exports = {
   mode: 'development',
-
-  entry: './app.js',
-
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-
+  entry: './src/entry-client.js',
   module: {
     rules: [
       {
@@ -22,13 +14,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['vue-style-loader', 'css-loader', 'postcss-loader']
-        // use: ExtractTextPlugin.extract({
-        //   fallback: 'vue-style-loader',
-        //   use: [åç
-        //     'css-loader',
-        //     'postcss-loader'
-        //   ]
-        // })
       },
       {
         test: /\.(jpg|jpeg|png|gif|svg)$/,
@@ -45,12 +30,21 @@ module.exports = {
       }
     ]
   },
-
+  resolve: {
+    extensions: [
+      '.js', '.vue', '.json'
+    ],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
   plugins: [
     new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
+    new HtmlWebpackPlugin({template: './src/index.html'}),
     new ExtractTextPlugin("styles.css")
-  ]
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
