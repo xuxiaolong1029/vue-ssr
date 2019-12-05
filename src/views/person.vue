aa8996b93c3268ba07e5fab55b98b9f6dca27771
xuxl <xuxl@citex.io>
2019-12-05 18:35:52 +0800

    commit

--- src/views/person.vue
+++ src/views/person.vue
@@ -8,14 +8,14 @@
 export default {
   head () {
     return{
-      title:' | 个人',
+      title:'个人',
       author:'徐小笼',
       keywords:'citex交易所,citex exchange',
       description:'支持比特币（BTC）/莱特币（LTC）/以太坊（ETH）等多种数字货币交易，高速撮合引擎、全方位资金安全保障及极速充提体验，为您的交易保驾护航.'
     }
   },
   methods: {
-    
+
   },
 }
 </script>
