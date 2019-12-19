e8eace807f253e81577b36c18b7f5f286dc866ff
xuxl <xuxl@citex.io>
2019-12-19 18:17:41 +0800

    commit

--- src/views/order.vue
+++ src/views/order.vue
@@ -1,23 +1,6 @@
 <template>
     <div>
-        <el-table
-            :data="tableData"
-            style="width: 100%;">
-        <el-table-column
-                prop="date"
-                label="日期"
-                width="180">
-        </el-table-column>
-        <el-table-column
-                prop="name"
-                label="姓名"
-                width="180">
-        </el-table-column>
-        <el-table-column
-                prop="address"
-                label="地址">
-        </el-table-column>
-    </el-table>
+        订单页面
     </div>
 </template>
 
